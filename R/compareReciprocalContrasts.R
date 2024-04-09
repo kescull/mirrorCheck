b8cd5174a526781741a06f5146652867c13dabaa
kescull <kate.scull@monash.edu>
2024-04-09 14:53:01 +1000

    Initial commit

---
+++ R/compareReciprocalContrasts.R
@@ -0,0 +1,226 @@
+# newFromList function was sourced from github UpsetR issue 85, solution 
+# provided by 'docmanny' in September 2017
+newFromList <- function (input) {
+  # Same as original fromList()...
+  elements <- unique(unlist(input))
+  data <- unlist(lapply(input, function(x) {
+    x <- as.vector(match(elements, x))
+  }))
+  data[is.na(data)] <- as.integer(0)
+  data[data != 0] <- as.integer(1)
+  data <- data.frame(matrix(data, ncol = length(input), byrow = F))
+  data <- data[which(rowSums(data) != 0), ]
+  names(data) <- names(input)
+  # ... Except now it conserves your original value names!
+  row.names(data) <- elements
+  return(data)
+}
+
+collapse.reciprocal.results <- function(group1,group2, folder) {
+  #get csvs
+  fns <- c(paste0(group1,"_vs_",group2,"_DG.csv"),paste0(group2,"_vs_",group1,"_DG.csv"))
+  simple.titles <- c(paste0("Against_",group2),paste0("Against_",group1))
+  paths <- file.path(folder,fns)
+  stopifnot(all(file.exists(paths)))
+  
+  reciprocal.results <- lapply(paths,read.csv)
+  names(reciprocal.results) <- simple.titles
+  reciprocal.results <- lapply(reciprocal.results, function(x) x %>% 
+                                 dplyr::select(-X) %>% 
+                                 dplyr::mutate(reg = dplyr::if_else(log2FoldChange > 0, "UP","DOWN")))
+  #Venn
+  gene.lists <- lapply(reciprocal.results,function(x) x %>% dplyr::pull(name))
+  invisible(futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger"))
+  venn.plot <- VennDiagram::venn.diagram(x = gene.lists, 
+                            category.names = names(gene.lists),
+                            filename = NULL,
+                            imagetype="png",
+                            cat.prompts=TRUE,
+                            margin=0.2,
+                            cat.dist=rep(0.1,length(gene.lists)),
+                            ext.dist=rep(-0.1,length(gene.lists)))
+  grid::grid.newpage()
+  grid::grid.draw(venn.plot)
+  
+  partitions <- VennDiagram::get.venn.partitions(gene.lists)
+  
+  #merge results tables
+  merged <- reciprocal.results %>% 
+    purrr::reduce(dplyr::full_join, by="name", suffix = simple.titles) %>%
+    tidyr::unite(combo, tidyselect::starts_with("reg"), remove = F) %>%
+    dplyr::mutate(agree = dplyr::if_else(combo == "UP_DOWN" | combo == "DOWN_UP", T,F),
+           LFC.diff = rowSums(dplyr::across(tidyselect::starts_with("log2FoldChange"))),
+           padj.diff = purrr::reduce(dplyr::across(tidyselect::starts_with("padj")), `-`),
+           group = paste(group1,group2,sep=".")) %>%
+    dplyr::select(-combo)
+  
+  overlap <- merged %>% dplyr::filter(!is.na(padj.diff))
+  bad <- overlap %>% dplyr::filter(agree == F)
+  if (!nrow(bad) == 0) {
+    print("Found some that didn't agree on the direction of regulation")
+    print(bad)
+  }
+  if (nrow(overlap) == 0) {
+    print(paste("No overlap of regulated genes using different reference level for",
+                group1,"and",group2))
+  } else {
+    write.csv(overlap,file.path(folder,paste0("consensusDG_",group1,"_v_",group2,".csv")))
+  }
+  
+  for.plotting <- merged %>% 
+    dplyr::mutate(partition = dplyr::case_when(!is.na(padj.diff) ~ "consensus",
+                                 name %in% partitions$..values..[[2]] ~ "group1ref",
+                                 name %in% partitions$..values..[[3]] ~ "group2ref"))
+}
+
+compare.pairwise.recursively <- function(groups, i, j,folder,res.list=NULL,list.ind = 0) {
+  if (is.null(res.list)) {
+    res.list <- list()
+  }
+  if (i > 1) {
+    if (j > 0 ) {
+      list.ind <- list.ind + 1
+      res.list[[list.ind]] <- collapse.reciprocal.results(groups[i],groups[j],folder)
+      res.list <- Recall(groups,i,j-1,folder, res.list, list.ind)
+    } else {
+      res.list <- Recall(groups,i-1,i-2,folder,res.list, list.ind)
+    }
+  } 
+  res.list
+}
+
+#' Compare results from reciprocal contrasts
+#'
+#' This finds all the 'pairs' among the DG csvs created by
+#' [run.DESeq.all.contrasts()] and looks for differences in the results found
+#' when using each level as the reference level.
+#'
+#' @section Rationale:
+#'
+#'   Let the pair of levels in question be called group1 and group2. Then the
+#'   genes found to be upregulated when using group1 as the reference should be
+#'   downregulated when group2 is the reference, and vice versa. Also the
+#'   magnitude of the log fold change for a gene should be nearly equal for both
+#'   sets of results, although with the opposite sign; thus the sum of the two
+#'   log fold changes for each gene is expected to be distributed around zero.
+#'   Furthermore, there should be a strong consensus set of significantly
+#'   up/down-regulated genes i.e. high degree of overlap between the genes seen
+#'   using group1 as the reference and those seen using group2. However,
+#'   [lfcShrink()] can sometimes produce quite different results for a contrast
+#'   depending which level you use as reference, and from our observation is
+#'   then more likely to find upregulated genes than down-regulated genes, which
+#'   leads to low consensus between the results sets and sum(log fold changes)
+#'   density plots which are skewed/shifted to the right. This function produces
+#'   1. simple Venn diagrams showing the overlap in results for the pairwise
+#'   comparisons, collected in one pdf named Venns.pdf; 
+#'   2) diagnostic plots to visualise how well the analysis worked, including 
+#'   density plots of the sums of the log fold changes per gene, and stacked bar 
+#'   plots summarising the Venn diagrams so that the strength of the consensus 
+#'   can be judged readily;
+#'   3) csv tables containing only the consensus genes for each pair of levels;
+#'   4) Finally, the overlap in the identities of significant genes between
+#'   different pairwise contrasts may be of interest. The function uses UpSetR
+#'   to plot and print the intersections between the consensus DEG sets from
+#'   each pairwise contrast.
+#'
+#' @param groups character vector specifying the names of the levels in the
+#'   factor used for the DESeq analysis by [run.DESeq.all.contrasts()]. i.e. if
+#'   [run.DESeq.all.contrasts()] had the parameters `dds = dds, condition = 'set'`,
+#'   then here you could use `groups = levels(dds$set)`
+#' @param folder character string with the file path to the folder of results
+#'   from [run.DESeq.all.contrasts()]. Output figures and tables will also be
+#'   deposited here.
+#'
+#' @return named list of data.frames representing merged tables from each 'pair'
+#'   of contrasts. Also creates pdf and csv files in the specified folder.
+#'
+#' @export
+compare.reciprocal.contrasts <- function(groups, folder) {
+  # Compare all reciprocal pairs to get consensus DEGs and print Venns
+  pdf(file.path(folder,"Venns.pdf"))
+  res.list <- compare.pairwise.recursively(levels(as.factor(groups)),
+                                           length(groups),
+                                           length(groups)-1,
+                                           folder)
+  dev.off()
+  names(res.list) <- lapply(res.list,function (x) x$group[[1]])
+  
+  #How many significant genes overall? Make UpSet and intersections
+  listInput <- lapply(res.list, function(x) x %>%
+                        dplyr::filter(!is.na(padj.diff)) %>%
+                        dplyr::pull(name))
+  originalSetNames <- names(listInput)
+  setNames <- gsub("\\.","_v_", originalSetNames)
+  names(listInput) <- setNames
+  intersections <- newFromList(listInput)
+  write.csv(intersections, 
+            file = file.path(folder,"all_contrasts_DEG_intersections.csv"), 
+            na="")
+  
+  setNames <- gsub("\\.","\n", originalSetNames)
+  names(listInput) <- setNames
+  
+  upsetPlot <- UpSetR::upset(UpSetR::fromList(listInput), nsets = length(listInput),
+                     order.by = "freq",mainbar.y.label = "DEG Intersections", 
+                     sets.x.label = "DEGs Per Contrast")
+  
+  # Plot all graphs of summed LFC per gene from each contrast using alternate group as reference level 
+  ready.for.lfc.check <- lapply(res.list,function(x) x %>% 
+                                  dplyr::filter(!is.na(padj.diff)) %>% 
+                                  dplyr::select(LFC.diff,group))
+  all <- dplyr::bind_rows(ready.for.lfc.check)
+  all$group <- as.factor(all$group) 
+  group.levels <- levels(all$group) 
+  group.labels <- gsub("\\.","\n", group.levels)
+  names(group.labels) <- group.levels
+  xlim <- max(abs(min(all$LFC.diff)),abs(max(all$LFC.diff)))
+  if (xlim > 0.5) xlim = 0.5
+  densityPlot <- ggplot2::ggplot(all, ggplot2::aes(x = LFC.diff, group = group)) +
+    ggplot2::geom_density() + 
+    ggplot2::facet_wrap(~group, ncol = 3, labeller = ggplot2::labeller(group = group.labels)) +
+    ggplot2::xlim(c(-xlim,xlim)) +
+    ggplot2::xlab("Sum of the 2 LFCs for the contrast\nwhen using each group as reference level in turn") 
+  
+  # Plot stacked bar charts of overlap for each contrast
+  ready.for.overlap.check <- lapply(res.list,function(x) x %>% 
+                                      dplyr::select(partition) %>%
+                                      dplyr::mutate(total = dplyr::n()) %>%
+                                      dplyr::group_by(partition) %>%
+                                      dplyr::summarise(n = dplyr::n(),
+                                                total = sum(total)/n,
+                                                pc = n/total *100))
+  
+  all.overlap <- dplyr::bind_rows(ready.for.overlap.check, .id = "group") 
+  
+  group.labels <- gsub("\\.","\n", all.overlap$group)
+  names(group.labels) <- all.overlap$group
+  legend.labels <- c("consensus","only with group1 as ref","only with group2 as ref")
+  
+  barPlot <- ggplot2::ggplot(all.overlap, 
+                             ggplot2::aes(x = group, y = pc, fill = partition)) +
+    ggplot2::geom_bar(stat = "identity") +
+    ggplot2::xlab(ggplot2::element_blank()) +
+    ggplot2::ylab("Percentage of differentially expressed genes") +
+    ggplot2::scale_x_discrete(labels=group.labels) +
+    ggplot2::scale_fill_discrete(labels = legend.labels) + 
+    ggplot2::geom_text(ggplot2::aes(label = n),
+                       position = ggplot2::position_stack(vjust = 0.5),size = 3) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(legend.position = "top") +
+    ggplot2::guides(fill = ggplot2::guide_legend(title = ggplot2::element_blank(), 
+                                                 reverse=T)) +
+    ggplot2::scale_fill_manual(values = c('#EE7733', '#0077BB', '#33BBEE')) +
+    ggplot2::coord_flip() 
+  
+  #Print output graphs
+  pdf(file.path(folder,
+                "diagnostic plots for contrasts with different ref level.pdf"), 
+      paper = "a4")
+  print(densityPlot)
+  print(barPlot)
+  print(upsetPlot)
+  dev.off()
+  
+  #return data
+  res.list
+}
---
+++ R/runDESeqAllContrasts.R
@@ -0,0 +1,205 @@
+plot_Volcano <- function(r,title, p.cutoff = 0.1, fc.cutoff = 2, top.n = 30) {
+  padj <- as.data.frame(r) %>%
+    dplyr::filter(!is.na(padj)) %>%
+    dplyr::mutate(log10padj = -log10(padj),
+           diff.expressed = dplyr::if_else(log2FoldChange > fc.cutoff & padj < p.cutoff, "UP",
+                                           dplyr::if_else(log2FoldChange < -fc.cutoff & padj < p.cutoff, 
+                                            "DOWN","NO"))) %>%
+    tibble::rownames_to_column("g_symbol")
+  diff.ex <- padj %>% dplyr::filter(diff.expressed != "NO")
+  
+  sums <- table(diff.ex$diff.expressed)
+  maxy <- max(padj$log10padj)
+  minx <- min(padj$log2FoldChange)
+  maxx <- max(padj$log2FoldChange)
+  padj$labels <- ifelse(padj$g_symbol %in% head(diff.ex[order(diff.ex$padj), 
+                                                        "g_symbol"], top.n), 
+                        padj$g_symbol, NA)
+  group.colors <- c(DOWN = "#00AFBB",NO = "grey",UP = "#FFDB6D")
+  group.labels <- c(DOWN = "Downregulated",NO = "Not significant", UP = "Upregulated")
+  
+  p <- ggplot2::ggplot(padj, ggplot2::aes(x = log2FoldChange, 
+                        y = log10padj, 
+                        col = diff.expressed,
+                        label = labels)) +
+    ggplot2::theme_classic() +
+    ggplot2::labs(title = title,
+         x = expression("log"[2]*"FC"), y = expression("-log"[10]*"adj-pvalue")) +
+    ggplot2::geom_vline(xintercept = c(-fc.cutoff, fc.cutoff), col = "gray", linetype = 'dashed') +
+    ggplot2::geom_hline(yintercept = -log10(p.cutoff), col = "gray", linetype = 'dashed') +
+    ggplot2::geom_point(size = 1) +
+    ggplot2::scale_color_manual(values = group.colors, 
+                       labels = group.labels) +
+    ggrepel::geom_text_repel(max.overlaps = Inf, size = 3, color = "black", na.rm = T) +
+    ggplot2::annotate("text",x = minx + 2, y = maxy - 1, label = paste("DOWN",sums["DOWN"])) +
+    ggplot2::annotate("text",x = maxx - 1, y = maxy - 1, label = paste("UP",sums["UP"]))
+}
+
+get.diff.genes.list <- function(res, p.cutoff = 0.1, fc.cutoff = 2) {
+  diff.ex <- as.data.frame(res)  %>%
+    dplyr::filter(!is.na(padj)) %>%
+    dplyr::mutate(log10padj = -log10(padj),
+           diff.expressed = dplyr::if_else(log2FoldChange > fc.cutoff & padj < p.cutoff, "UP",
+                                           dplyr::if_else(log2FoldChange < -fc.cutoff & padj < p.cutoff, 
+                                            "DOWN","NO"))) %>%
+    tibble::rownames_to_column("g_symbol") %>% 
+    dplyr::filter(diff.expressed != "NO")
+  diff.ex$g_symbol
+}
+
+plot_heatmap <- function(dg.list,title,dds,annotation.col) {
+  vsd <- DESeq2::vst(dds)
+  df <- as.data.frame(SummarizedExperiment::colData(dds)) %>%
+    dplyr::select(dplyr::all_of(annotation.col))
+  p <- pheatmap::pheatmap(SummarizedExperiment::assay(vsd)[rownames(vsd) %in% dg.list,], cluster_rows=F, show_rownames=FALSE,
+                cluster_cols=T, annotation_col = df, main = title)
+}
+
+get.all.results <- function(i,dds, mode = "lfcShrink", alpha = 0.1) {
+  coef = DESeq2::resultsNames(dds)[i+1]
+  res <- DESeq2::results(dds, 
+                         name = DESeq2::resultsNames(dds)[i+1], 
+                         alpha = alpha)
+  if (mode == "lfcShrink") {
+    res <- DESeq2::lfcShrink(dds, coef=i+1, type="apeglm", res = res)
+  }
+  resOrdered <- res[order(res$padj),]
+}
+#' @export
+get.all.MA.plots <- function(res,coef,p.cutoff = 0.1) {
+  MA <- DESeq2::plotMA(res, ylim=c(-3,3), main = coef,alpha = p.cutoff)
+}
+
+write.diff.genes <- function(res,dg,coef, folder) {
+  res.sig <- as.data.frame(res) %>% 
+    tibble::rownames_to_column("name") %>% 
+    dplyr::filter(name %in% dg)
+  fn <- file.path(folder,paste0(coef[1],"_DG.csv"))
+  write.csv(res.sig,file = fn)
+}
+
+# taken from is.integer() documentation
+is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)  {
+  abs(x - round(x)) < tol
+}
+
+#' Run DESeq for all contrasts
+#'
+#' Runs [DESeq()] for a `DESeqDataSet` with a simple design. It uses each
+#' possible reference level in turn, extracting all contrast results for each.
+#'
+#' @section Further details: Output includes pdf files with DESeq2 MA plots,
+#'   volcano plots and heatmaps of significant genes for each contrast in one
+#'   pdf file per reference level, and also a csv of the significant genes for
+#'   each contrast (according to the user-specified adj-pvalue and lfc
+#'   thresholds). It is possible to run with more complex designs for the first
+#'   reference level using the `only.first.iteration` parameter, but this has
+#'   not been tested thoroughly and output should be interpreted with care.
+#'
+#' @param dds Valid `DESeqDataSet` object with a simple design e.g. `design =
+#'   ~condition`. If you wish to use a filter such as `edgeR` [filterByExpr()],
+#'   please do so prior to running this function. This package is intended to
+#'   help assess the usefulness of such filtering.
+#' @param folder character string specifying the relative or absolute filepath
+#'   of the folder where you want the output files to go. This folder should
+#'   exist.
+#' @param mode either `"noShrink"` or `"lfcShrink"`. Specifies whether to use
+#'   [results()] or [lfcShrink()] (default is `"lfcShrink"`).
+#' @param condition character string related to the design formula used when
+#'   creating the `DESeqDataSet`. This should be a `colData` variable in the
+#'   `DESeqDataSet`; e.g. for `design=~group`, here enter `condition="group"`.
+#'   If you use a more complex design, results are not guaranteed. See also
+#'   `only.first.iteration`.
+#' @param heatmap.annotation.col character vector specifying which `colData`
+#'   variables to use as groups in the heatmap output. If NULL, the heatmap will
+#'   use the variable specified in `condition` (default NULL)
+#' @param p.cutoff numeric. The significance cut-off for p-adjusted values,
+#'   equivalent to parameter alpha in [DESeq2::results()]. (default 0.1)
+#' @param fc.cutoff numeric. The log2 fold change significance cut-off; this is
+#'   used in the Volcano plots and when printing out significant genes. It is
+#'   NOT used to change the null hypothesis in [DESeq2::results()]. (default 2)
+#' @param top.n whole number. Number of most highly regulated genes to label
+#'   with gene symbols on Volcano plots. (default 30).
+#' @param only.first.iteration If True, the function will process all the
+#'   contrasts using the first level as the reference level, then exit. This
+#'   should produce 1 set of csvs for contrast DGEs and 1 pdf with MA and
+#'   Volcano plots. It may be helpful for looking at more complex designs, so
+#'   long as `condition` can still be specified as a `colData` variable. But no
+#'   promises. (default False)
+#'
+#' @return None - but creates pdf and csv files in the specified folder.
+#'
+#' @export
+run.DESeq.all.contrasts <- function(dds,folder, 
+                                    mode = "lfcShrink", 
+                                    condition,
+                                    heatmap.annotation.col = NULL,
+                                    p.cutoff = 0.1, 
+                                    fc.cutoff = 2, 
+                                    top.n = 30,
+                                    only.first.iteration = F) {
+  if (!mode %in% c("lfcShrink","noShrink")) {
+    stop("mode must be lfcShrink or noShrink (default is lfcShrink)")
+  }
+  
+  if (!is.numeric(p.cutoff) | !is.numeric(fc.cutoff) | !is.wholenumber(top.n)) {
+    stop(paste(p.cutoff,"p.cutoff and fc.cutoff must be numeric; top.n must be a whole number"))
+  }
+  if (!condition %in% colnames(SummarizedExperiment::colData(dds))) {
+    stop("'condition' should be a variable from colData(dds)")
+  }
+  groups <- levels(dds[[ {{condition}} ]])
+  if(any(!heatmap.annotation.col %in% colnames(SummarizedExperiment::colData(dds)))) {
+    stop("heatmap.annotation.col must be a vector of character strings chosen from among those found by calling colData() on the given DESeqDataSet")
+  }
+  if (!file.exists(file.path(folder))) {
+    stop("specified output folder should exist, please make it or check your filepath")
+  }
+  for (g in groups) {
+    dds[[ {{condition}} ]] <- relevel(dds[[ {{condition}} ]], ref = g)
+    if (length(DESeq2::resultsNames(dds)) < 2) {
+      #first time, use first group and run DESeq
+      dds <- DESeq2::DESeq(dds)
+    } else {
+      dds <- DESeq2::nbinomWaldTest(dds)
+    }
+    
+    coef <- DESeq2::resultsNames(dds)[2:length(DESeq2::resultsNames(dds))]
+    prefix <- paste0(condition,"_")
+    coef <- sub(prefix,"",coef)
+    plots.fn <- file.path(folder,paste0("v_",g,"_DGEplots.pdf"))
+    
+    #get all results and plot
+    resList <- lapply(1:length(coef),
+                      get.all.results, 
+                      dds = dds, mode = mode, alpha = p.cutoff)
+    
+    pdf(plots.fn, paper = "a4")
+    par(mfrow = c(3,2))
+    mapply(get.all.MA.plots,resList,coef, MoreArgs = list(p.cutoff = p.cutoff))
+    volcList <- mapply(plot_Volcano,resList,coef,
+                       MoreArgs = list(p.cutoff = p.cutoff, 
+                                       fc.cutoff = fc.cutoff, 
+                                       top.n = top.n), 
+                       SIMPLIFY = F)
+    print(volcList)
+    dgList <- lapply(resList, get.diff.genes.list, 
+                     p.cutoff = p.cutoff, fc.cutoff = fc.cutoff)
+    if (!is.null(heatmap.annotation.col)) {
+      heatmaps <- mapply(plot_heatmap,dgList,coef,
+                         MoreArgs = list(dds = dds,
+                                         annotation.col = heatmap.annotation.col), 
+                         SIMPLIFY = F)
+    } else {
+      heatmaps <- mapply(plot_heatmap,dgList,coef,
+                         MoreArgs = list(dds = dds,
+                                         annotation.col = condition), 
+                         SIMPLIFY = F)
+    }
+    dev.off()
+    mapply(write.diff.genes,resList,dgList,coef, MoreArgs = list(folder = folder))
+    if (only.first.iteration) {
+      return()
+    }
+  }  
+}
